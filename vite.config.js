546ad6328aa1534ad0ed21eec9154b2ffaf46653
M31ancholy <zdc6651@163.com>
2024-02-16 10:05:19 +0800

    111

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base:'/Stop_convoluting/', // 自己添加的东西
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
