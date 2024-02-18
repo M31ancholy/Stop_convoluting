9428cd13a8738129621441d82cc4d1bc95e4adcf
M31ancholy <zdc6651@163.com>
2024-02-18 17:21:52 +0800

    Signed-off-by: M31ancholy <zdc6651@163.com>

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import test_button from './components/test_button.vue'
 </script>
 
 <template>
-  <div class = "BackGroundCSS">  </div>
-  <div class="main">
-    <img src="./components/Img/Pic_Dong.jpg" class="header">
-    <p style="color: white;">现在你有一个机会，只要按下这个按钮，你就能窃取到董神的一小部分智商</p>
-    <test_button></test_button>
+  <div class = "black-background">
+    <div class="main">
+      <img src="./components/Img/Pic_Dong.jpg" class="header">
+      <p style="color: white;">现在你有一个机会，只要按下这个按钮，你就能窃取到董神的一小部分智商</p>
+      <test_button></test_button>
     </div>
+  </div>
+
 </template>
 
 <style scoped>
@@ -25,14 +27,16 @@ import test_button from './components/test_button.vue'
   z-index: -1;
   position: absolute;
 }
-.BackGroundCSS {
-  width: 100vh;
-  height: 100vh;
-  background: url("./components/Img/blackPic.jpg") center center no-repeat;
-  background-size: 100% 100%;
-  position: absolute;
-  z-index: -1;
-}
+.black-background {
+    background-color: #000; /* 黑色背景 */
+    width: 100vw; /* 视口宽度 */
+    height: 100vh; /* 视口高度 */
+    position: fixed; /* 使用固定定位 */
+    top: 0;
+    left: 0;
+    z-index: -1; /* 确保它位于其他内容之下 */
+  }
+
 .main{
   z-index: 1;
 }
